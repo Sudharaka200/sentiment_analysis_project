3253aea7dcc5c79894c92f97a39648ad17b7f542
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-02-25 00:35:38 +0530

    logs done

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from helper import preprocessing, vectorizer, get_prediction
+from logger import logging
 
 app = Flask(__name__)
+
+logging.info('Flask server started')
 
 data = dict()
 reviews = ['Good Product', 'Bad Product', 'I like it']
@@ -14,15 +17,25 @@ def index():
     data['reviews'] =  reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('------Open Home Page-----------')
+
     return render_template('index.html', data=data)
 
 
 @app.route("/", methods = ['post'])
 def my_post():
     text = request.form['text']
+    logging.info(f'Text : {text}')
+
     preprocessed_txt = preprocessing(text)
+    logging.info(f'Text : {preprocessed_txt}')
+
     vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'Text : {vectorized_txt}')
+
     predection =  get_prediction(vectorized_txt)
+    logging.info(f'Text : {predection}')
 
     if predection == 'negative':
         global negative
